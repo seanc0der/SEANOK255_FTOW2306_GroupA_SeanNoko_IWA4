9d06e884b2d7e836096734dcceb0c4507929a5fb
seanc0der <sean.noko@gmail.com>
2023-08-07 01:04:45 +0200

    updated the js file code by fixing the syntax errors and amending the code where necessary. I added an html file and linked it to the .js file to be able to also run the browser console.

--- rsa-public-holidays-iwa4.js
+++ rsa-public-holidays-iwa4.js
@@ -1,35 +1,33 @@
-const date: 2050
-const status: 'student'
-const count = 0
+const date = 2050;
+const person_status = "student";
+let count = 0;
 
-if (date = 2050) {
-	console.log("January", 'New Year’s Day'
-	console.log("March", 'Human Rights Day')
-	date = 'April'
-	console.log(date, 'Family Day')
-	console.log(date, 'Freedom Day')
-	let count = count + 4
+if (date === 2050) {
+	console.log("January", "New Year's Day");
+	console.log("March", "Human Rights Day");
+	console.log("April", "Family Day");
+	console.log("April", "Freedom Day");
+	count = count + 4;
 
-	if (status = "student") {
-	  console.log('June', 'Youth Day')
-		let count = count + 1
-  }
+	if (person_status === "student") {
+		console.log("June", "Youth Day");
+		count = count + 1;
+	}
 
-	console.log('August', 'Women’s Day')
-	console.log('September', 'Heritage Day')
-	date = 'December'
-	console.log(date, 'Day of Reconciliation')
-	let count = count + 3
+	console.log("August", "Women's Day");
+	console.log("September", "Heritage Day");
+	console.log("December", "Day of Reconciliation");
+	count = count + 3;
 
-	if (status = "parent") {
-	  console.log(date, 'Christmas Day')
-		let count = count + 1
-  }
+	if (person_status === "parent") {
+		console.log("December", "Christmas Day");
+		let count = count + 1;
+	}
 
-	console.log(date, 'Day of Goodwill')
-	let count = count + 1
+	console.log("December", "Day of Goodwill");
+	count = count + 1;
 }
 
-console.log('Your status is:', status)
-console.log('The year is:', date)
-console.log('The total holidays is:', count)
+console.log("Your status is:", person_status);
+console.log("The year is:", date);
+console.log("The total holidays is:", count);
